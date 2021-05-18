c2b10c94a4151896c073d7a63149fad2f2a15c6d
DWVoid <yshliu0321@icloud.com>
2021-05-19 00:39:38 +0800

    Bug Fix and Reduce Code Length

--- native/Source/copy.cpp
+++ native/Source/copy.cpp
@@ -5,93 +5,43 @@
 static void *ptr(const jlong mem) noexcept { return reinterpret_cast<void *>(static_cast<uintptr_t>(mem)); }
 
 static void JNICALL copyB2N(JNIEnv *e, jclass, jbyteArray src, jlong dst, jint size) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(src, nullptr);
-    std::memmove(ptr(dst), m, size);
-    e->ReleasePrimitiveArrayCritical(src, m, JNI_ABORT);
+    const auto c_src = e->GetPrimitiveArrayCritical(src, nullptr);
+    std::memmove(ptr(dst), c_src, size);
+    e->ReleasePrimitiveArrayCritical(src, c_src, JNI_ABORT);
 }
 
 static void JNICALL copyN2B(JNIEnv *e, jclass, jlong src, jint size, jbyteArray dst) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(dst, nullptr);
-    std::memmove(m, ptr(src), size);
-    e->ReleasePrimitiveArrayCritical(dst, m, 0);
+    const auto c_dst = e->GetPrimitiveArrayCritical(dst, nullptr);
+    std::memmove(c_dst, ptr(src), size);
+    e->ReleasePrimitiveArrayCritical(dst, c_dst, 0);
 }
 
-static void JNICALL copyS2N(JNIEnv *e, jclass, jshortArray src, jlong dst, jint size, jboolean le) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(src, nullptr);
+template<class T>
+static void JNICALL copyM2U(JNIEnv *e, jclass, jarray src, jlong dst, jint size, jboolean le) noexcept {
+    const auto c_src = e->GetPrimitiveArrayCritical(src, nullptr);
     if (le && native_le) {
-        std::memmove(ptr(dst), m, size);
+        std::memmove(ptr(dst), c_src, size * sizeof(T));
     } else {
-        auto l = static_cast<uint16_t *>(ptr(dst));
-        auto r = static_cast<uint8_t *>(m);
-        const auto end = l + size;
-        for (; l < end; r += sizeof(uint16_t), ++l) *l = load16(r);
+        auto l = static_cast<uint8_t *>(ptr(dst));
+        auto r = static_cast<T *>(c_src);
+        const auto end = r + size;
+        for (; r < end; ++r, l += sizeof(T)) store(l, *r);
     }
-    e->ReleasePrimitiveArrayCritical(src, m, JNI_ABORT);
+    e->ReleasePrimitiveArrayCritical(src, c_src, JNI_ABORT);
 }
 
-static void JNICALL copyN2S(JNIEnv *e, jclass, jlong src, jint size, jshortArray dst, jboolean le) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(dst, nullptr);
+template<class T>
+static void JNICALL copyU2M(JNIEnv *e, jclass, jlong src, jint size, jarray dst, jboolean le) noexcept {
+    const auto c_dst = e->GetPrimitiveArrayCritical(dst, nullptr);
     if (le && native_le) {
-        std::memmove(m, ptr(src), size);
+        std::memmove(c_dst, const_cast<const void *>(ptr(src)), size * sizeof(T));
     } else {
-        auto l = static_cast<uint8_t *>(m);
-        auto r = static_cast<uint16_t *>(ptr(src));
+        auto l = static_cast<T *>(c_dst);
+        auto r = static_cast<uint8_t *>(ptr(src));
         const auto end = l + size;
-        for (; l < end; r += sizeof(uint16_t), ++l) store16(*r, l);
+        for (; l < end; ++l, r += sizeof(T)) load(*l, r);
     }
-    e->ReleasePrimitiveArrayCritical(dst, m, 0);
-}
-
-static void JNICALL copyI2N(JNIEnv *e, jclass, jintArray src, jlong dst, jint size, jboolean le) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(src, nullptr);
-    if (le && native_le) {
-        std::memmove(ptr(dst), m, size);
-    } else {
-        auto r = static_cast<uint8_t *>(m);
-        auto l = static_cast<uint32_t *>(ptr(dst));
-        const auto end = l + size;
-        for (; l < end; r += sizeof(uint32_t), ++l) *l = load32(r);
-    }
-    e->ReleasePrimitiveArrayCritical(src, m, JNI_ABORT);
-}
-
-static void JNICALL copyN2I(JNIEnv *e, jclass, jlong src, jint size, jintArray dst, jboolean le) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(dst, nullptr);
-    if (le && native_le) {
-        std::memmove(m, ptr(src), size);
-    } else {
-        auto l = static_cast<uint8_t *>(m);
-        auto r = static_cast<uint32_t *>(ptr(src));
-        const auto end = l + size;
-        for (; l < end; r += sizeof(uint32_t), ++l) store32(*r, l);
-    }
-    e->ReleasePrimitiveArrayCritical(dst, m, 0);
-}
-
-static void JNICALL copyL2N(JNIEnv *e, jclass, jlongArray src, jlong dst, jint size, jboolean le) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(src, nullptr);
-    if (le && native_le) {
-        std::memmove(ptr(dst), m, size);
-    } else {
-        auto r = static_cast<uint8_t *>(m);
-        auto l = static_cast<uint64_t *>(ptr(dst));
-        const auto end = l + size;
-        for (; l < end; r += sizeof(uint64_t), ++l) *l = load64(r);
-    }
-    e->ReleasePrimitiveArrayCritical(src, m, JNI_ABORT);
-}
-
-static void JNICALL copyN2L(JNIEnv *e, jclass, jlong src, jint size, jlongArray dst, jboolean le) noexcept {
-    const auto m = e->GetPrimitiveArrayCritical(dst, nullptr);
-    if (le && native_le) {
-        std::memmove(m, ptr(src), size);
-    } else {
-        auto l = static_cast<uint8_t *>(m);
-        auto r = static_cast<uint64_t *>(ptr(src));
-        const auto end = l + size;
-        for (; l < end; r += sizeof(uint64_t), ++l) store64(*r, l);
-    }
-    e->ReleasePrimitiveArrayCritical(dst, m, 0);
+    e->ReleasePrimitiveArrayCritical(dst, c_dst, 0);
 }
 
 static_assert(std::numeric_limits<float>::is_iec559);
@@ -104,19 +54,20 @@ static void *erase(T fn) noexcept { return reinterpret_cast<void *>(fn); }
 static char *c(const char *s) noexcept { return const_cast<char *>(s); }
 
 void registerArrayKt(JNIEnv *e) noexcept {
+    static char *const name = c("copy");
     const JNINativeMethod methods[] = {
-            {c("copy"), c("([BJI)V"),  erase(copyB2N)},
-            {c("copy"), c("(JI[B)V"),  erase(copyN2B)},
-            {c("copy"), c("([SJIZ)V"), erase(copyS2N)},
-            {c("copy"), c("(JI[SZ)V"), erase(copyN2S)},
-            {c("copy"), c("([IJIZ)V"), erase(copyI2N)},
-            {c("copy"), c("(JI[IZ)V"), erase(copyN2I)},
-            {c("copy"), c("([JJIZ)V"), erase(copyL2N)},
-            {c("copy"), c("(JI[JZ)V"), erase(copyN2L)},
-            {c("copy"), c("([FJIZ)V"), erase(copyI2N)},
-            {c("copy"), c("(JI[FZ)V"), erase(copyN2I)},
-            {c("copy"), c("([DJIZ)V"), erase(copyL2N)},
-            {c("copy"), c("(JI[DZ)V"), erase(copyN2L)}
+            {name, c("([BJI)V"),  erase(copyB2N)},
+            {name, c("(JI[B)V"),  erase(copyN2B)},
+            {name, c("([SJIZ)V"), erase(copyM2U<uint16_t>)},
+            {name, c("(JI[SZ)V"), erase(copyU2M<uint16_t>)},
+            {name, c("([IJIZ)V"), erase(copyM2U<uint32_t>)},
+            {name, c("(JI[IZ)V"), erase(copyU2M<uint32_t>)},
+            {name, c("([JJIZ)V"), erase(copyM2U<uint64_t>)},
+            {name, c("(JI[JZ)V"), erase(copyU2M<uint64_t>)},
+            {name, c("([FJIZ)V"), erase(copyM2U<uint32_t>)},
+            {name, c("(JI[FZ)V"), erase(copyU2M<uint32_t>)},
+            {name, c("([DJIZ)V"), erase(copyM2U<uint64_t>)},
+            {name, c("(JI[DZ)V"), erase(copyU2M<uint64_t>)}
     };
     const auto clazz = e->FindClass("site/neworld/cio/unsafe/ArrayKt");
     e->RegisterNatives(clazz, methods, 12);
--- native/Source/endian.h
+++ native/Source/endian.h
@@ -10,59 +10,56 @@
 
 constexpr bool native_le = std::endian::native == std::endian::little;
 
-inline uint16_t load16(const uint8_t* v) noexcept {
-    uint16_t ret;
+inline void load(uint16_t& ret, const uint8_t* v) noexcept {
     std::memcpy(&ret, v, sizeof(uint16_t));
 #ifdef _MSC_VER
-    return _byteswap_ushort(ret);
+    ret = _byteswap_ushort(ret);
 #else
-    return __builtin_bswap16(ret);
+    ret = __builtin_bswap16(ret);
 #endif
 }
 
-inline void store16(const uint16_t l, uint8_t* r) noexcept {
-#ifdef _MSC_VER
-    const auto val = _byteswap_ushort(l);
-#else
-    const auto val = __builtin_bswap16(l);
-#endif
-    std::memcpy(r, &val, sizeof(uint16_t));
-}
-
-inline uint32_t load32(const uint8_t* v) noexcept {
-    uint32_t ret;
+inline void load(uint32_t& ret, const uint8_t* v) noexcept {
     std::memcpy(&ret, v, sizeof(uint32_t));
 #ifdef _MSC_VER
-    return _byteswap_ushort(ret);
+    ret = _byteswap_ulong(ret);
 #else
-    return __builtin_bswap32(ret);
+    ret = __builtin_bswap32(ret);
 #endif
 }
 
-inline void store32(const uint32_t l, uint8_t* r) noexcept {
-#ifdef _MSC_VER
-    const auto val = _byteswap_ulong(l);
-#else
-    const auto val = __builtin_bswap32(l);
-#endif
-    std::memcpy(r, &val, sizeof(uint32_t));
-}
-
-inline uint64_t load64(const uint8_t* v) noexcept {
-    uint64_t ret;
+inline void load(uint64_t& ret, const uint8_t* v) noexcept {
     std::memcpy(&ret, v, sizeof(uint64_t));
 #ifdef _MSC_VER
-    return _byteswap_ushort(ret);
+    ret = _byteswap_uint64(ret);
 #else
-    return __builtin_bswap64(ret);
+    ret = __builtin_bswap64(ret);
 #endif
 }
 
-inline void store64(const uint64_t l, uint8_t* r) noexcept {
+inline void store(uint8_t* l, const uint16_t r) noexcept {
 #ifdef _MSC_VER
-    const auto val = _byteswap_ushort(l);
+    const auto v = _byteswap_ushort(r);
 #else
-    const auto val = __builtin_bswap64(l);
+    const auto v = __builtin_bswap16(l);
 #endif
-    std::memcpy(r, &val, sizeof(uint64_t));
+    std::memcpy(l, &v, sizeof(uint16_t));
+}
+
+inline void store(uint8_t* l, const uint32_t r) noexcept {
+#ifdef _MSC_VER
+    const auto v = _byteswap_ulong(r);
+#else
+    const auto v = __builtin_bswap32(l);
+#endif
+    std::memcpy(l, &v, sizeof(uint32_t));
+}
+
+inline void store(uint8_t* l, const uint64_t r) noexcept {
+#ifdef _MSC_VER
+    const auto v = _byteswap_uint64(r);
+#else
+    const auto v = __builtin_bswap64(l);
+#endif
+    std::memcpy(l, &v, sizeof(uint64_t));
 }
